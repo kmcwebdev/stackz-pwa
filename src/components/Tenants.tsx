aa99b0f04a43c40779fa7f6c4af17bc2d56608c7
jyzrprgrmmr <jayzurgandia@gmail.com>
2021-08-26 15:10:32 +0800

    Finished tenant component

---
+++ src/components/Tenants.tsx
@@ -0,0 +1,98 @@
+import classNames from 'classnames';
+import React from 'react';
+import { useWindowSize } from 'src/utils/use-window-size';
+
+export interface Tenant {
+  name: string;
+  address: string;
+  amount: string;
+}
+
+interface TenantsProps {
+  data: Tenant[];
+}
+
+const Tenants: React.FC<TenantsProps> = ({ data }) => {
+  const windowWidth = useWindowSize().width;
+
+  const containerWidth = classNames(
+    {
+      'w-full': data.length === 1,
+      'w-9/12 md:w-full': data.length > 1,
+    },
+    'flex'
+  );
+  const tenantCardWidth = classNames(
+    {
+      'w-full': data.length === 1,
+      'flex-1 md:w-6/12': data.length === 2,
+      'flex-1 md:w-4/12': data.length === 3,
+      'flex-1 md:w-3/12': data.length >= 4,
+    },
+    'h-full p-2 text-white bg-blue-400 border-2 border-gray-100 rounded-md'
+  );
+
+  const hiddenTenants = classNames(
+    {
+      hidden: data.length === 1,
+      'flex md:hidden': data.length === 2,
+    },
+    'relative w-3/12 h-full'
+  );
+
+  const hideTenant = (i: number): string => {
+    let result = '';
+    if (windowWidth > 768) {
+      if (i >= 4) {
+        result = 'hidden';
+      }
+    } else {
+      if (i >= 1) {
+        result = 'hidden';
+      }
+    }
+    return result;
+  };
+
+  return (
+    <>
+      <div className={containerWidth}>
+        {/* FLOOR TENANT = 1 */}
+        {data.map((tenant, i) => (
+          <div
+            className={`
+              ${tenantCardWidth}
+              ${data.length === 2 && i === 1 && 'hidden md:block'}
+              ${data.length === 3 && i >= 1 && 'hidden md:block'}
+              ${data.length >= 4 && hideTenant(i)}
+              `}
+          >
+            <div className='w-full overflow-ellipsis'>
+              <p className='mb-1 font-semibold capitalize text-xs md:text-md truncate'>
+                {tenant.name}
+              </p>
+              <p className='text-xs capitalize truncate'>{tenant.address}</p>
+              <p className='text-xs capitalize truncate'>{tenant.amount}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className={`${hiddenTenants} ${data.length <= 4 && 'md:hidden'}`}>
+        <div className='absolute z-50 w-full h-full transition-all border-2 border-gray-100 cursor-pointer hover:bg-gray-400 hover:bg-opacity-50'></div>
+        <div className='absolute z-30 grid h-full font-semibold text-white bg-blue-600 border-2 border-gray-100 rounded-md right-card1 place-items-center'>
+          +
+          {data.length > 4
+            ? windowWidth > 768
+              ? data.length - 4
+              : data.length - 1
+            : data.length - 1}
+        </div>
+        <div className='absolute z-20 grid h-full font-semibold text-white bg-red-400 border-2 border-gray-100 rounded-md right-card2 place-items-center'></div>
+        <div className='absolute grid h-full font-semibold text-white bg-blue-600 border-2 border-gray-100 rounded-md right-card3 place-items-center '></div>
+      </div>
+    </>
+  );
+};
+
+export default Tenants;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,107 +1,184 @@
+import Tenants from 'src/components/Tenants';
+
 const Dashboard: React.FC = () => {
+  const floorData = [
+    {
+      floorNo: 1,
+      tenants: [
+        {
+          name: 'Tenant 1',
+          address: 'Address 1',
+          amount: 'Amount 1',
+        },
+      ],
+    },
+    {
+      floorNo: 2,
+      tenants: [
+        {
+          name: 'Tenant 1',
+          address: 'Address 1',
+          amount: 'Amount 1',
+        },
+        {
+          name: 'Tenant 2',
+          address: 'Address 2',
+          amount: 'Amount 2',
+        },
+      ],
+    },
+    {
+      floorNo: 3,
+      tenants: [
+        {
+          name: 'Tenant 1',
+          address: 'Address 1',
+          amount: 'Amount 1',
+        },
+        {
+          name: 'Tenant 2',
+          address: 'Address 2',
+          amount: 'Amount 2',
+        },
+        {
+          name: 'Tenant 3',
+          address: 'Address 3',
+          amount: 'Amount 3',
+        },
+      ],
+    },
+    {
+      floorNo: 4,
+      tenants: [
+        {
+          name: 'Tenant 1',
+          address: 'Address 1',
+          amount: 'Amount 1',
+        },
+        {
+          name: 'Tenant 2',
+          address: 'Address 2',
+          amount: 'Amount 2',
+        },
+        {
+          name: 'Tenant 3',
+          address: 'Address 3',
+          amount: 'Amount 3',
+        },
+        {
+          name: 'Tenant 4',
+          address: 'Address 4',
+          amount: 'Amount 4',
+        },
+      ],
+    },
+    {
+      floorNo: 5,
+      tenants: [
+        {
+          name: 'Tenant 1',
+          address: 'Address 1',
+          amount: 'Amount 1',
+        },
+        {
+          name: 'Tenant 2',
+          address: 'Address 2',
+          amount: 'Amount 2',
+        },
+        {
+          name: 'Tenant 3',
+          address: 'Address 3',
+          amount: 'Amount 3',
+        },
+        {
+          name: 'Tenant 4',
+          address: 'Address 4',
+          amount: 'Amount 4',
+        },
+        {
+          name: 'Tenant 5',
+          address: 'Address 5',
+          amount: 'Amount 5',
+        },
+      ],
+    },
+
+    {
+      floorNo: 9,
+      tenants: [
+        {
+          name: 'Tenant 1',
+          address: 'Address 1',
+          amount: 'Amount 1',
+        },
+        {
+          name: 'Tenant 2',
+          address: 'Address 2',
+          amount: 'Amount 2',
+        },
+        {
+          name: 'Tenant 3',
+          address: 'Address 3',
+          amount: 'Amount 3',
+        },
+        {
+          name: 'Tenant 4',
+          address: 'Address 4',
+          amount: 'Amount 4',
+        },
+        {
+          name: 'Tenant 5',
+          address: 'Address 5',
+          amount: 'Amount 5',
+        },
+        {
+          name: 'Tenant 6',
+          address: 'Address 6',
+          amount: 'Amount 6',
+        },
+        {
+          name: 'Tenant 7',
+          address: 'Address 7',
+          amount: 'Amount 7',
+        },
+        {
+          name: 'Tenant 8',
+          address: 'Address 8',
+          amount: 'Amount 8',
+        },
+        {
+          name: 'Tenant 9',
+          address: 'Address 9',
+          amount: 'Amount 9',
+        },
+      ],
+    },
+  ];
+
   return (
     <div className='w-full'>
       <div className='relative flex m-2 space-x-2 '>
         <div className='w-20 px-2 text-xs font-semibold'> Floor</div>
         <div className='flex-1 px-2 text-xs font-semibold'> Tenant</div>
       </div>
-      {/* Floor 23 */}
-      <div className='relative flex h-20 m-2 space-x-2'>
-        {/* Floor Number */}
-        <div className='w-20 h-full'>
-          <div className='flex flex-col items-center justify-center h-full bg-gray-200 border-2 border-gray-100 rounded-md'>
-            <span className='text-xl font-semibold uppercase'>23</span>
-          </div>
-        </div>
 
-        {/* Floor Tenants */}
-        <div className='relative flex flex-1 w-20 h-full gap-x-2'>
-          <div className='w-9/12 h-full p-2 text-white bg-blue-400 border-2 border-gray-100 rounded-md'>
-            <p className='mb-1 font-semibold capitalize text-md'>
-              Insert Tenant Name
-            </p>
-            <p className='text-xs font-semibold capitalize'>
-              Insert Tenant Info 1
-            </p>
-            <p className='text-xs font-semibold capitalize'>
-              Insert Tenant Info 2
-            </p>
-          </div>
-          <div className='relative w-3/12 h-full'>
-            <div className='absolute z-50 w-full h-full transition-all border-2 border-gray-100 cursor-pointer hover:bg-gray-400 hover:bg-opacity-50'></div>
-            <div className='absolute z-30 grid h-full font-semibold text-white bg-blue-600 border-2 border-gray-100 rounded-md right-card1 place-items-center'>
-              +3
+      {floorData.map((floor) => (
+        <div className='relative flex h-20 m-2 space-x-1 md:space-x-2'>
+          {/* Floor Number */}
+          <div className='w-16 md:w-20 h-full'>
+            <div className='flex flex-col items-center justify-center h-full bg-gray-200 border-2 border-gray-100 rounded-md'>
+              <span className='text-xl font-semibold uppercase'>
+                {floor.floorNo}
+              </span>
             </div>
+          </div>
 
-            <div className='absolute z-20 grid h-full font-semibold text-white bg-red-400 border-2 border-gray-100 rounded-md right-card2 place-items-center'></div>
-
-            <div className='absolute grid h-full font-semibold text-white bg-blue-600 border-2 border-gray-100 rounded-md right-card3 place-items-center '></div>
+          {/* Floor Tenants */}
+          <div className='relative flex flex-1 w-20 h-full'>
+            <Tenants data={floor.tenants} />
           </div>
         </div>
-      </div>
-      {/* Floor 23*/}
-
-      {/* Floor Number */}
-      <div className='relative flex h-20 m-2 space-x-2'>
-        {/* Floor Number */}
-        <div className='w-20 h-full'>
-          <div className='flex flex-col items-center justify-center h-full bg-gray-200 border-2 border-gray-100 rounded-md'>
-            <span className='text-xl font-semibold uppercase'>24</span>
-          </div>
-        </div>
-
-        {/* Floor Tenants */}
-        <div className='relative flex flex-1 w-20 h-full gap-x-2'>
-          <div className='w-full h-full p-2 text-white bg-blue-400 border-2 border-gray-100 rounded-md'>
-            <p className='mb-1 font-semibold capitalize text-md'>
-              Insert Tenant Name
-            </p>
-            <p className='text-xs font-semibold capitalize'>
-              Insert Tenant Info 1
-            </p>
-            <p className='text-xs font-semibold capitalize'>
-              Insert Tenant Info 2
-            </p>
-          </div>
-        </div>
-      </div>
-      {/* Floor 24 */}
-
-      {/* Floor 25 */}
-      <div className='relative flex h-20 m-2 space-x-2'>
-        {/* Floor Number*/}
-        <div className='w-20 h-full'>
-          <div className='flex flex-col items-center justify-center h-full bg-gray-200 border-2 border-gray-100 rounded-md'>
-            <span className='text-xl font-semibold uppercase'>25</span>
-          </div>
-        </div>
-
-        {/* Floor Tenants */}
-        <div className='relative flex flex-1 w-20 h-full gap-x-2'>
-          <div className='w-9/12 h-full p-2 text-white bg-blue-400 border-2 border-gray-100 rounded-md'>
-            <p className='mb-1 font-semibold capitalize text-md'>
-              Insert Tenant Name
-            </p>
-            <p className='text-xs font-semibold capitalize'>
-              Insert Tenant Info 1
-            </p>
-            <p className='text-xs font-semibold capitalize'>
-              Insert Tenant Info 2
-            </p>
-          </div>
-          <div className='relative w-3/12 h-full'>
-            <div className='absolute z-50 w-full h-full transition-all border-2 border-gray-100 cursor-pointer hover:bg-gray-400 hover:bg-opacity-50'></div>
-            <div className='absolute z-30 grid h-full font-semibold text-white bg-blue-600 border-2 border-gray-100 rounded-md right-card1 place-items-center'>
-              +1
-            </div>
-
-            <div className='absolute z-20 grid h-full font-semibold text-white bg-red-400 border-2 border-gray-100 rounded-md right-card2 place-items-center'></div>
-
-            <div className='absolute grid h-full font-semibold text-white bg-blue-600 border-2 border-gray-100 rounded-md right-card3 place-items-center '></div>
-          </div>
-        </div>
-      </div>
-      {/* Floor 25 */}
+      ))}
     </div>
   );
 };
