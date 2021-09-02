835edd28e087beab161b4943968be9ab1c631775
jyzrprgrmmr <jayzurgandia@gmail.com>
2021-09-02 14:59:17 +0800

    Latest changes

--- src/pages/building/floor/UnitInfo.tsx
+++ src/pages/building/floor/UnitInfo.tsx
@@ -27,33 +27,10 @@ const UnitInfo: React.FC<UnitInfoProps> = ({ data }) => {
 
   const menu = (
     <Menu>
-      <Menu.Item>
-        <a
-          target='_blank'
-          rel='noopener noreferrer'
-          href='https://www.antgroup.com'
-        >
-          1st menu item
-        </a>
-      </Menu.Item>
-      <Menu.Item>
-        <a
-          target='_blank'
-          rel='noopener noreferrer'
-          href='https://www.aliyun.com'
-        >
-          2nd menu item
-        </a>
-      </Menu.Item>
-      <Menu.Item>
-        <a
-          target='_blank'
-          rel='noopener noreferrer'
-          href='https://www.luohanacademy.com'
-        >
-          3rd menu item
-        </a>
-      </Menu.Item>
+      <Menu.Item>1st menu item</Menu.Item>
+      <Menu.Item>2nd menu item</Menu.Item>
+      <Menu.Item>3rd menu item</Menu.Item>
+      <Menu.Item className='hidden lg:block'>Submit Feedback</Menu.Item>
     </Menu>
   );
 
