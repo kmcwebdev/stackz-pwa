c2430f414ff20c599c2fe6dee8a77b830cd58990
jyzrgandia <jayzur.gandia@kmc.solutions>
2021-09-13 14:54:56 +0800

    changed header button color

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -20,11 +20,11 @@ const Header: React.FC<HeaderProps> = ({ hasExportButton, headerTitle }) => {
       <div className='relative px-4 z-10 flex flex-shrink-0 items-center justify-between h-16 border-b border-gray-200'>
         <button
           type='button'
-          className='text-gray-500 focus:outline-none md:hidden'
+          className='focus:outline-none md:hidden text-primary'
           onClick={() => setSidebarOpen(!sidebarOpen)}
         >
           <span className='sr-only'>Open sidebar</span>
-          <MenuAlt1Icon className='w-5 h-5 text-primary' aria-hidden='true' />
+          <MenuAlt1Icon className='w-5 h-5' aria-hidden='true' />
         </button>
 
         <div className='flex items-center justify-center text-center font-medium text-primary'>
